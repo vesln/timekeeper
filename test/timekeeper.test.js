3c68f0adc37c00cfc0aee79df5a8e29d45a51f81
Ben Doherty <ben@thinkoomph.com>
2016-03-11 11:04:08 -0500

    Refactor / consolidate test case resets

--- test/timekeeper.test.js
+++ test/timekeeper.test.js
@@ -28,30 +28,28 @@ describe('TimeKeeper', function() {
         tk.freeze(this.time);
       });
 
+      afterEach(function() {
+        tk.reset();
+      });
+
       it('freezes the time create with `new Date` to the supplied one', function() {
         sleep(10);
         var date = new Date;
         date.getTime().should.eql(this.time.getTime());
-        tk.reset();
       });
 
       it('freezes the time create with `Date#now` to the supplied one', function() {
         sleep(10);
         Date.now().should.eql(this.time.getTime());
-        tk.reset();
       });
 
       it('should not affect other date calls', function() {
         tk.freeze(this.time);
         (new Date(1330688329320)).getTime().should.eql(1330688329320);
-        tk.reset();
       });
 
       it('should return distinct frozen date objects', function() {
-				var date = new Date(), date1 = new Date();
-
-				date.should.not.equal(date1);
-        tk.reset();
+        (new Date()).should.not.equal(new Date());
       });
     });
   });
