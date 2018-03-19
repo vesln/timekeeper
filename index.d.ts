1e37cbc509dbb0bfd27cc4fecdf6f78d72afc03e
Mark S. Everitt <mark.s.everitt@gmail.com>
2018-03-19 14:23:49 +0000

    Adds a typescript declaration file.

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export as namespace timekeeper;
+
+export function freeze() : void;
+export function travel(date: Date) : void;
+export function reset() : void;
+export function isKeepingTime() : boolean;
